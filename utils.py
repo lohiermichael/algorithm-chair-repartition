839008eb736987db7a66cde2fe776a6f12638b3b
Michael Lohier <lohiermichael@gmail.com>
2020-07-20 16:36:08 +0800

    Add function to get the root in utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+
+
 def console_print_of(apartment) -> None:
     list_of_strings = list(map(''.join, apartment))
     print('\n'.join(list_of_strings))
+
+
+def get_project_root() -> Path:
+    """Returns project root folder."""
+    return Path(__file__).parent
